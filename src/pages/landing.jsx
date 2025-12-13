9ec2f321111240a4542c47b8dc636c1d9a133f1a
Henry Emily <henryandcharlie123@gmail.com>
2025-12-13 20:55:32 +0700

    add

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -12,9 +12,22 @@ import watchImage from "../images/bg_landing.png";
 import logo from "../images/tissot_logo_1.png";
 import bgElement from "../images/bg_elemen.png";
 import marcMarquezWatch from "../images/marc_marquez_watch.png";
+import MotoGP2025 from "../images/MotoGP_2025.png";
+// import Trace33mm from "../images/T-Race_33mm.png"
+// import Trace41mm from "../images/T-Race_41mm.png"
+// import Trace38mm from "../images/T-Race_38mm.png"
+// import Trace45mm from "../images/T-Race_45mm.png"
+// import Valjoux from "../images/valjoux.png"
+// import Powermatic from "../images/powermatic_80.png"
+// import Mechanical from "../images/mechanical.png"
+// import History1 from "../images/history_1.png"
+// import History2 from "../images/history_2.png"
+// import History3 from "../images/history_3.png"
+// import History4 from "../images/history_4.png"
+// import bgElementv2 from "../images/bg_elemenv2.png";
 
 function Landing() {
-    const size = 1.2;
+    const size = 1.5;
     return (
         <Flex   
             align="center"
@@ -118,16 +131,45 @@ function Landing() {
                                 bgPos="left"
                                 bgRepeat="no-repeat"
                                 bgSize="contain"
+                            >
+                                
+                            </Flex>
+                            <Text
+                                fontSize="16px"
+                                mx={2}
+                                textAlign="end"
+                                w="100%"
+                            >
+                                Marc Marquez
+                            </Text>
+                        </Flex>
+                    </WrapItem>
+                    <WrapItem ml={4}>
+                        <Flex
+                            align="end"
+                            justify="center"
+                            bgImg={`url(${bgElement})`}
+                            bgPos="center"
+                            bgRepeat="no-repeat"
+                            bgSize="cover"
+                            h={`${98*size}px`}
+                            w={`${163*size}px`}
+                        >
+                            <Flex
+                                align="end"
+                                bgImg={`url(${MotoGP2025})`}
+                                bgRepeat="no-repeat"
+                                bgSize="contain"
                                 h="100%"
                                 w="100%"
                             >
                                 <Text
                                     fontSize="16px"
-                                    mx={3}
+                                    mx={2}
                                     textAlign="right"
                                     w="100%"
                                 >
-                                    Marc Marquez
+                                    &nbsp;
                                 </Text>
                             </Flex>
                         </Flex>
